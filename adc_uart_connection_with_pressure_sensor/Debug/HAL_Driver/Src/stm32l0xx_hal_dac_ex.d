44f0f1d73eee09f8c9c5e4daedd9b2f58a8f479f
Antonio Takev <tonitakev@gmail.com>
2020-02-20 12:10:24 +0200

    making a program for getting data from pressure sensor and push it to the computer via UART interface whilst the air pump pumping the cuff, using PWM for managing the power of air pump. The air pum on/off mode is managed by a USER button on the board STM32-Nucleo L010RB, which is used in the current project

---
+++ adc_uart_connection_with_pressure_sensor/Debug/HAL_Driver/Src/stm32l0xx_hal_dac_ex.d
@@ -0,0 +1,2 @@
+HAL_Driver/Src/stm32l0xx_hal_dac_ex.o: \
+ ../HAL_Driver/Src/stm32l0xx_hal_dac_ex.c
---
+++ adc_uart_connection_with_pressure_sensor/Debug/HAL_Driver/Src/subdir.mk
@@ -0,0 +1,220 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../HAL_Driver/Src/stm32l0xx_hal.c \
+../HAL_Driver/Src/stm32l0xx_hal_adc.c \
+../HAL_Driver/Src/stm32l0xx_hal_adc_ex.c \
+../HAL_Driver/Src/stm32l0xx_hal_comp.c \
+../HAL_Driver/Src/stm32l0xx_hal_comp_ex.c \
+../HAL_Driver/Src/stm32l0xx_hal_cortex.c \
+../HAL_Driver/Src/stm32l0xx_hal_crc.c \
+../HAL_Driver/Src/stm32l0xx_hal_crc_ex.c \
+../HAL_Driver/Src/stm32l0xx_hal_cryp.c \
+../HAL_Driver/Src/stm32l0xx_hal_cryp_ex.c \
+../HAL_Driver/Src/stm32l0xx_hal_dac.c \
+../HAL_Driver/Src/stm32l0xx_hal_dac_ex.c \
+../HAL_Driver/Src/stm32l0xx_hal_dma.c \
+../HAL_Driver/Src/stm32l0xx_hal_firewall.c \
+../HAL_Driver/Src/stm32l0xx_hal_flash.c \
+../HAL_Driver/Src/stm32l0xx_hal_flash_ex.c \
+../HAL_Driver/Src/stm32l0xx_hal_flash_ramfunc.c \
+../HAL_Driver/Src/stm32l0xx_hal_gpio.c \
+../HAL_Driver/Src/stm32l0xx_hal_i2c.c \
+../HAL_Driver/Src/stm32l0xx_hal_i2c_ex.c \
+../HAL_Driver/Src/stm32l0xx_hal_i2s.c \
+../HAL_Driver/Src/stm32l0xx_hal_irda.c \
+../HAL_Driver/Src/stm32l0xx_hal_iwdg.c \
+../HAL_Driver/Src/stm32l0xx_hal_lcd.c \
+../HAL_Driver/Src/stm32l0xx_hal_lptim.c \
+../HAL_Driver/Src/stm32l0xx_hal_msp_template.c \
+../HAL_Driver/Src/stm32l0xx_hal_pcd.c \
+../HAL_Driver/Src/stm32l0xx_hal_pcd_ex.c \
+../HAL_Driver/Src/stm32l0xx_hal_pwr.c \
+../HAL_Driver/Src/stm32l0xx_hal_pwr_ex.c \
+../HAL_Driver/Src/stm32l0xx_hal_rcc.c \
+../HAL_Driver/Src/stm32l0xx_hal_rcc_ex.c \
+../HAL_Driver/Src/stm32l0xx_hal_rng.c \
+../HAL_Driver/Src/stm32l0xx_hal_rtc.c \
+../HAL_Driver/Src/stm32l0xx_hal_rtc_ex.c \
+../HAL_Driver/Src/stm32l0xx_hal_smartcard.c \
+../HAL_Driver/Src/stm32l0xx_hal_smartcard_ex.c \
+../HAL_Driver/Src/stm32l0xx_hal_smbus.c \
+../HAL_Driver/Src/stm32l0xx_hal_spi.c \
+../HAL_Driver/Src/stm32l0xx_hal_tim.c \
+../HAL_Driver/Src/stm32l0xx_hal_tim_ex.c \
+../HAL_Driver/Src/stm32l0xx_hal_tsc.c \
+../HAL_Driver/Src/stm32l0xx_hal_uart.c \
+../HAL_Driver/Src/stm32l0xx_hal_uart_ex.c \
+../HAL_Driver/Src/stm32l0xx_hal_usart.c \
+../HAL_Driver/Src/stm32l0xx_hal_wwdg.c \
+../HAL_Driver/Src/stm32l0xx_ll_adc.c \
+../HAL_Driver/Src/stm32l0xx_ll_comp.c \
+../HAL_Driver/Src/stm32l0xx_ll_crc.c \
+../HAL_Driver/Src/stm32l0xx_ll_crs.c \
+../HAL_Driver/Src/stm32l0xx_ll_dac.c \
+../HAL_Driver/Src/stm32l0xx_ll_dma.c \
+../HAL_Driver/Src/stm32l0xx_ll_exti.c \
+../HAL_Driver/Src/stm32l0xx_ll_gpio.c \
+../HAL_Driver/Src/stm32l0xx_ll_i2c.c \
+../HAL_Driver/Src/stm32l0xx_ll_lptim.c \
+../HAL_Driver/Src/stm32l0xx_ll_lpuart.c \
+../HAL_Driver/Src/stm32l0xx_ll_pwr.c \
+../HAL_Driver/Src/stm32l0xx_ll_rcc.c \
+../HAL_Driver/Src/stm32l0xx_ll_rng.c \
+../HAL_Driver/Src/stm32l0xx_ll_rtc.c \
+../HAL_Driver/Src/stm32l0xx_ll_spi.c \
+../HAL_Driver/Src/stm32l0xx_ll_tim.c \
+../HAL_Driver/Src/stm32l0xx_ll_usart.c \
+../HAL_Driver/Src/stm32l0xx_ll_usb.c \
+../HAL_Driver/Src/stm32l0xx_ll_utils.c 
+
+OBJS += \
+./HAL_Driver/Src/stm32l0xx_hal.o \
+./HAL_Driver/Src/stm32l0xx_hal_adc.o \
+./HAL_Driver/Src/stm32l0xx_hal_adc_ex.o \
+./HAL_Driver/Src/stm32l0xx_hal_comp.o \
+./HAL_Driver/Src/stm32l0xx_hal_comp_ex.o \
+./HAL_Driver/Src/stm32l0xx_hal_cortex.o \
+./HAL_Driver/Src/stm32l0xx_hal_crc.o \
+./HAL_Driver/Src/stm32l0xx_hal_crc_ex.o \
+./HAL_Driver/Src/stm32l0xx_hal_cryp.o \
+./HAL_Driver/Src/stm32l0xx_hal_cryp_ex.o \
+./HAL_Driver/Src/stm32l0xx_hal_dac.o \
+./HAL_Driver/Src/stm32l0xx_hal_dac_ex.o \
+./HAL_Driver/Src/stm32l0xx_hal_dma.o \
+./HAL_Driver/Src/stm32l0xx_hal_firewall.o \
+./HAL_Driver/Src/stm32l0xx_hal_flash.o \
+./HAL_Driver/Src/stm32l0xx_hal_flash_ex.o \
+./HAL_Driver/Src/stm32l0xx_hal_flash_ramfunc.o \
+./HAL_Driver/Src/stm32l0xx_hal_gpio.o \
+./HAL_Driver/Src/stm32l0xx_hal_i2c.o \
+./HAL_Driver/Src/stm32l0xx_hal_i2c_ex.o \
+./HAL_Driver/Src/stm32l0xx_hal_i2s.o \
+./HAL_Driver/Src/stm32l0xx_hal_irda.o \
+./HAL_Driver/Src/stm32l0xx_hal_iwdg.o \
+./HAL_Driver/Src/stm32l0xx_hal_lcd.o \
+./HAL_Driver/Src/stm32l0xx_hal_lptim.o \
+./HAL_Driver/Src/stm32l0xx_hal_msp_template.o \
+./HAL_Driver/Src/stm32l0xx_hal_pcd.o \
+./HAL_Driver/Src/stm32l0xx_hal_pcd_ex.o \
+./HAL_Driver/Src/stm32l0xx_hal_pwr.o \
+./HAL_Driver/Src/stm32l0xx_hal_pwr_ex.o \
+./HAL_Driver/Src/stm32l0xx_hal_rcc.o \
+./HAL_Driver/Src/stm32l0xx_hal_rcc_ex.o \
+./HAL_Driver/Src/stm32l0xx_hal_rng.o \
+./HAL_Driver/Src/stm32l0xx_hal_rtc.o \
+./HAL_Driver/Src/stm32l0xx_hal_rtc_ex.o \
+./HAL_Driver/Src/stm32l0xx_hal_smartcard.o \
+./HAL_Driver/Src/stm32l0xx_hal_smartcard_ex.o \
+./HAL_Driver/Src/stm32l0xx_hal_smbus.o \
+./HAL_Driver/Src/stm32l0xx_hal_spi.o \
+./HAL_Driver/Src/stm32l0xx_hal_tim.o \
+./HAL_Driver/Src/stm32l0xx_hal_tim_ex.o \
+./HAL_Driver/Src/stm32l0xx_hal_tsc.o \
+./HAL_Driver/Src/stm32l0xx_hal_uart.o \
+./HAL_Driver/Src/stm32l0xx_hal_uart_ex.o \
+./HAL_Driver/Src/stm32l0xx_hal_usart.o \
+./HAL_Driver/Src/stm32l0xx_hal_wwdg.o \
+./HAL_Driver/Src/stm32l0xx_ll_adc.o \
+./HAL_Driver/Src/stm32l0xx_ll_comp.o \
+./HAL_Driver/Src/stm32l0xx_ll_crc.o \
+./HAL_Driver/Src/stm32l0xx_ll_crs.o \
+./HAL_Driver/Src/stm32l0xx_ll_dac.o \
+./HAL_Driver/Src/stm32l0xx_ll_dma.o \
+./HAL_Driver/Src/stm32l0xx_ll_exti.o \
+./HAL_Driver/Src/stm32l0xx_ll_gpio.o \
+./HAL_Driver/Src/stm32l0xx_ll_i2c.o \
+./HAL_Driver/Src/stm32l0xx_ll_lptim.o \
+./HAL_Driver/Src/stm32l0xx_ll_lpuart.o \
+./HAL_Driver/Src/stm32l0xx_ll_pwr.o \
+./HAL_Driver/Src/stm32l0xx_ll_rcc.o \
+./HAL_Driver/Src/stm32l0xx_ll_rng.o \
+./HAL_Driver/Src/stm32l0xx_ll_rtc.o \
+./HAL_Driver/Src/stm32l0xx_ll_spi.o \
+./HAL_Driver/Src/stm32l0xx_ll_tim.o \
+./HAL_Driver/Src/stm32l0xx_ll_usart.o \
+./HAL_Driver/Src/stm32l0xx_ll_usb.o \
+./HAL_Driver/Src/stm32l0xx_ll_utils.o 
+
+C_DEPS += \
+./HAL_Driver/Src/stm32l0xx_hal.d \
+./HAL_Driver/Src/stm32l0xx_hal_adc.d \
+./HAL_Driver/Src/stm32l0xx_hal_adc_ex.d \
+./HAL_Driver/Src/stm32l0xx_hal_comp.d \
+./HAL_Driver/Src/stm32l0xx_hal_comp_ex.d \
+./HAL_Driver/Src/stm32l0xx_hal_cortex.d \
+./HAL_Driver/Src/stm32l0xx_hal_crc.d \
+./HAL_Driver/Src/stm32l0xx_hal_crc_ex.d \
+./HAL_Driver/Src/stm32l0xx_hal_cryp.d \
+./HAL_Driver/Src/stm32l0xx_hal_cryp_ex.d \
+./HAL_Driver/Src/stm32l0xx_hal_dac.d \
+./HAL_Driver/Src/stm32l0xx_hal_dac_ex.d \
+./HAL_Driver/Src/stm32l0xx_hal_dma.d \
+./HAL_Driver/Src/stm32l0xx_hal_firewall.d \
+./HAL_Driver/Src/stm32l0xx_hal_flash.d \
+./HAL_Driver/Src/stm32l0xx_hal_flash_ex.d \
+./HAL_Driver/Src/stm32l0xx_hal_flash_ramfunc.d \
+./HAL_Driver/Src/stm32l0xx_hal_gpio.d \
+./HAL_Driver/Src/stm32l0xx_hal_i2c.d \
+./HAL_Driver/Src/stm32l0xx_hal_i2c_ex.d \
+./HAL_Driver/Src/stm32l0xx_hal_i2s.d \
+./HAL_Driver/Src/stm32l0xx_hal_irda.d \
+./HAL_Driver/Src/stm32l0xx_hal_iwdg.d \
+./HAL_Driver/Src/stm32l0xx_hal_lcd.d \
+./HAL_Driver/Src/stm32l0xx_hal_lptim.d \
+./HAL_Driver/Src/stm32l0xx_hal_msp_template.d \
+./HAL_Driver/Src/stm32l0xx_hal_pcd.d \
+./HAL_Driver/Src/stm32l0xx_hal_pcd_ex.d \
+./HAL_Driver/Src/stm32l0xx_hal_pwr.d \
+./HAL_Driver/Src/stm32l0xx_hal_pwr_ex.d \
+./HAL_Driver/Src/stm32l0xx_hal_rcc.d \
+./HAL_Driver/Src/stm32l0xx_hal_rcc_ex.d \
+./HAL_Driver/Src/stm32l0xx_hal_rng.d \
+./HAL_Driver/Src/stm32l0xx_hal_rtc.d \
+./HAL_Driver/Src/stm32l0xx_hal_rtc_ex.d \
+./HAL_Driver/Src/stm32l0xx_hal_smartcard.d \
+./HAL_Driver/Src/stm32l0xx_hal_smartcard_ex.d \
+./HAL_Driver/Src/stm32l0xx_hal_smbus.d \
+./HAL_Driver/Src/stm32l0xx_hal_spi.d \
+./HAL_Driver/Src/stm32l0xx_hal_tim.d \
+./HAL_Driver/Src/stm32l0xx_hal_tim_ex.d \
+./HAL_Driver/Src/stm32l0xx_hal_tsc.d \
+./HAL_Driver/Src/stm32l0xx_hal_uart.d \
+./HAL_Driver/Src/stm32l0xx_hal_uart_ex.d \
+./HAL_Driver/Src/stm32l0xx_hal_usart.d \
+./HAL_Driver/Src/stm32l0xx_hal_wwdg.d \
+./HAL_Driver/Src/stm32l0xx_ll_adc.d \
+./HAL_Driver/Src/stm32l0xx_ll_comp.d \
+./HAL_Driver/Src/stm32l0xx_ll_crc.d \
+./HAL_Driver/Src/stm32l0xx_ll_crs.d \
+./HAL_Driver/Src/stm32l0xx_ll_dac.d \
+./HAL_Driver/Src/stm32l0xx_ll_dma.d \
+./HAL_Driver/Src/stm32l0xx_ll_exti.d \
+./HAL_Driver/Src/stm32l0xx_ll_gpio.d \
+./HAL_Driver/Src/stm32l0xx_ll_i2c.d \
+./HAL_Driver/Src/stm32l0xx_ll_lptim.d \
+./HAL_Driver/Src/stm32l0xx_ll_lpuart.d \
+./HAL_Driver/Src/stm32l0xx_ll_pwr.d \
+./HAL_Driver/Src/stm32l0xx_ll_rcc.d \
+./HAL_Driver/Src/stm32l0xx_ll_rng.d \
+./HAL_Driver/Src/stm32l0xx_ll_rtc.d \
+./HAL_Driver/Src/stm32l0xx_ll_spi.d \
+./HAL_Driver/Src/stm32l0xx_ll_tim.d \
+./HAL_Driver/Src/stm32l0xx_ll_usart.d \
+./HAL_Driver/Src/stm32l0xx_ll_usb.d \
+./HAL_Driver/Src/stm32l0xx_ll_utils.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+HAL_Driver/Src/%.o: ../HAL_Driver/Src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m0plus -mthumb -mfloat-abi=soft -DSTM32 -DSTM32L0 -DSTM32L010RBTx -DNUCLEO_L010RB -DDEBUG -DSTM32L010xB -DUSE_HAL_DRIVER -I"/home/toni/workspac_ac6/adc_uart_connection_with_pressure_sensor/HAL_Driver/Inc/Legacy" -I"/home/toni/workspac_ac6/adc_uart_connection_with_pressure_sensor/Utilities" -I"/home/toni/workspac_ac6/adc_uart_connection_with_pressure_sensor/inc" -I"/home/toni/workspac_ac6/adc_uart_connection_with_pressure_sensor/CMSIS/device" -I"/home/toni/workspac_ac6/adc_uart_connection_with_pressure_sensor/CMSIS/core" -I"/home/toni/workspac_ac6/adc_uart_connection_with_pressure_sensor/HAL_Driver/Inc" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
