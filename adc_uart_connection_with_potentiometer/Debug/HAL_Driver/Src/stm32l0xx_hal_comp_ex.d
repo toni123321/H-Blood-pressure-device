ef0f409095e84de8ad28344496e4119fcfaf32f3
toni123321 <tonitakev@gmail.com>
2020-03-23 12:27:31 +0200

    Remove program, because it has already new path

--- adc_uart_connection_with_potentiometer/Debug/HAL_Driver/Src/stm32l0xx_hal_comp_ex.d
+++
@@ -1,2 +0,0 @@
-HAL_Driver/Src/stm32l0xx_hal_comp_ex.o: \
- ../HAL_Driver/Src/stm32l0xx_hal_comp_ex.c
--- adc_uart_connection_with_potentiometer/Debug/HAL_Driver/Src/stm32l0xx_ll_lpuart.d
+++
@@ -1,2 +0,0 @@
-HAL_Driver/Src/stm32l0xx_ll_lpuart.o: \
- ../HAL_Driver/Src/stm32l0xx_ll_lpuart.c
