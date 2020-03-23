30535e84f1b9ad80441e9106f82dedb5d51b3061
toni123321 <tonitakev@gmail.com>
2020-03-23 12:11:35 +0200

    Remove folder Serial_connection, because organising purposes

--- Serial_connection/others/matlab_engine_test.py
+++
@@ -1,9 +0,0 @@
-
-import matlab.engine
-eng = matlab.engine.connect_matlab('my_engine') 
-
-filename = '20200308-225050.csv'
-[output1, output2, output3] = eng.main3(filename,nargout=3)
-
-
-print("{}/{}".format(int(output1), int(output3)))
--- Serial_connection/serial_name.py
+++
@@ -1,5 +0,0 @@
-import serial
-
-ser = serial.Serial('COM3')
-print(ser.name)
-ser.close()
