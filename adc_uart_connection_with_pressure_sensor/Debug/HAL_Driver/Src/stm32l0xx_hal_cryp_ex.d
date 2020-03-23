b868be4a2ded1747271b552bb7b7827a7632fe4f
toni123321 <tonitakev@gmail.com>
2020-03-23 12:29:10 +0200

    Remove program Delete the file for the selected program, because it has a new path

--- adc_uart_connection_with_pressure_sensor/Debug/HAL_Driver/Src/stm32l0xx_hal_cryp_ex.d
+++
@@ -1,2 +0,0 @@
-HAL_Driver/Src/stm32l0xx_hal_cryp_ex.o: \
- ../HAL_Driver/Src/stm32l0xx_hal_cryp_ex.c
--- adc_uart_connection_with_pressure_sensor/Debug/HAL_Driver/Src/stm32l0xx_hal_dac_ex.d
+++
@@ -1,2 +0,0 @@
-HAL_Driver/Src/stm32l0xx_hal_dac_ex.o: \
- ../HAL_Driver/Src/stm32l0xx_hal_dac_ex.c
